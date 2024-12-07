69212f8d27c1caf17ac3e0c6599febcb525f217f
pietervb <vbogaert.pieter@gmail.com>
2024-12-07 19:58:55 +0100

    app globally

--- app.py
+++ app.py
@@ -62,7 +62,8 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.getenv("PORT", 5000))  # Heroku provides the PORT, locally defaults to 5000
     app.run(debug=True, host="0.0.0.0", port=port)
